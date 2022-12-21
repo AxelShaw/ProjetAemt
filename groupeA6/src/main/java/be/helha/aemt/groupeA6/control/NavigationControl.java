68beea61f2d8e54f5d1a69a5e40bd21119bf4bbb
PierreGillebert <114138883+PierreGillebert@users.noreply.github.com>
2022-12-21 12:06:25 +0100

    Create NavigationControl.java

---
+++ groupeA6/src/main/java/be/helha/aemt/groupeA6/control/NavigationControl.java
@@ -0,0 +1,34 @@
+package be.helha.aemt.groupeA6.control;
+
+import java.io.Serializable;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Named;
+
+// scope == durée d'activité du controller (session, requete)
+@Named
+@SessionScoped
+public class NavigationControl implements Serializable {
+	
+	private String nom;
+	
+	public String doNext() {
+		return "next.xhtml";
+	}
+	
+	public String doList() {
+		return "list.xhtml";
+	}
+
+	public String doAdd() {
+		return "add.xhtml";
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+}
