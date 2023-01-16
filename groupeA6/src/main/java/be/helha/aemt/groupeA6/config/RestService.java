e707f1bc1096bd8a302a990c7b1132916d64c763
PierreGillebert <114138883+PierreGillebert@users.noreply.github.com>
2023-01-16 10:34:02 +0100

    init
    init

--- groupeA6/src/main/java/be/helha/aemt/groupeA6/config/RestService.java
+++
@@ -1,9 +0,0 @@
-package be.helha.aemt.groupeA6.config;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/enseignants")
-public class RestService extends Application {
-	
-}
